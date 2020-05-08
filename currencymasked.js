27199fd2fa0ad7a6a634f3fd52796f47fdd7f1c4
Aulia Khalqillah <52442929+auliakhalqillah@users.noreply.github.com>
2020-05-08 11:40:59 +0700

    Update currencymasked.js

--- currencymasked.js
+++ currencymasked.js
@@ -70,13 +70,13 @@ export default class App extends React.Component {
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
+  container: {
+    flex: 1,
     flexDirection: 'column',
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center'
-	},
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
   buttonContainer:{
     flexDirection: 'row'
   },
